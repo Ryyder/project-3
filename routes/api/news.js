c09fda2f01be5fb8ca3b96bc6795674534a3aef0
kdublam <kdublam@gmail.com>
2019-07-19 17:03:32 -0700

    news api to get top news on candidate or all news if no top news is returned

---
+++ routes/api/news.js
@@ -0,0 +1,22 @@
+const axios = require("axios");
+const router = require("express").Router();
+
+let name = //retrieve name from DB
+
+router.get("/news", (req, res) => {
+  axios
+    .get("https://newsapi.org/v2/top-headlines?q=" + name + "&apiKey=a7f110578c3c478b87d1d9c4c0477600", { params: req.query })
+    if(res.data.totalResults === 0){
+      axios
+      .get("https://newsapi.org/v2/everything?q="+ name + "&apiKey=a7f110578c3c478b87d1d9c4c0477600", { params: req.query })
+      .then(({ data: { results } }) => res.json(results))
+      .catch(err => res.status(422).json(err));
+    }else{
+      axios
+      .then(({ data: { results } }) => res.json(results))
+      .catch(err => res.status(422).json(err));
+    }
+    
+});
+
+module.exports = router;

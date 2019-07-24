28aa5eb382b7318f33226dbea9b67563eee2e064
kevinfukuhara <kevinfukuhara@gmail.com>
2019-07-24 14:39:15 -0700

    Made the seed file create a collection of candidates within the authenticaiton db - since the project uses only one db

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -5,7 +5,7 @@ const db = require("../models");
 
 mongoose.connect(
 	process.env.MONGODB_URI ||
-	"mongodb://localhost/reactcandidates"
+	"mongodb://localhost/authentication"
 );
 
 const candidateSeed = [

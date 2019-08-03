4d88cad5d29e877db5f55032fed33ead327c8b7b
kdublam <kdublam@gmail.com>
2019-08-03 13:33:03 -0700

    fixed candidate page so it's not stretching

--- client/src/components/BackButton/index.js
+++ client/src/components/BackButton/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 // import Router from 'react-router';
 import { Link } from "react-router-dom";
 import BackButton from '@material-ui/icons/ArrowBackIos';
+import "./style.css"
 
 
 export default function ButtonRouter(props) {
   return (
-      <Link to={`/${props.party}`}><BackButton /></Link>
+      <Link to={`/${props.party}`}><BackButton className="backBtn" /></Link>
   );
 }
